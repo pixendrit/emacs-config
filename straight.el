69ac9886112487b5e2dfd783b4406fc3f891227e
Pixendrit <pixendrit@outlook.com>
2022-03-17 18:42:23 +0100

    huh

---
+++ straight.el
@@ -0,0 +1,4 @@
+;; [[file:config.org::*Tailwind intellisense][Tailwind intellisense:3]]
+(straight-use-package
+ '(lsp-tailwindcss :type git :host github :repo "merrickluo/lsp-tailwindcss"))
+;; Tailwind intellisense:3 ends here
